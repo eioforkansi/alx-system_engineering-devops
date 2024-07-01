5c076da68374cb0b42e3afb8f949cfb3d6990397
eioforkansi <eioforkansi@gmail.com>
2024-07-01 08:21:44 +0100

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,34 +1,16 @@
-# This Puppet manifest sets up client SSH configuration file so that you can connect to a server without typing a password.
+# Seting up my client config file
+include stdlib
 
-file {  '/root/.ssh/config':
-    ensure => file,
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Host configuration':
-    path => '/root/.ssh/config',
-    line => 'Host ubuntu@18.210.15.231',
-}
-
-file_line { 'Set HostName':
-    path  => '/root/.ssh/config',
-    line  => '   HostName 18.210.15.231',
-    match => '^  HostName',
-}
-
-file_line { 'Set User':
-    path  => '/root/.ssh/config',
-    line  => '  User ubuntu',
-    match => '^ User',
-}
-
-file_line { 'Set IdentityFile':
-    path  => '/root/.ssh/config',
-    line  => '  IdentityFile ~/.ssh/school',
-    match => '^  IdentityFile',
-}
-
-file_line { 'Disable Password Authentication':
-    path  => '/root/.ssh/config',
-    line  => '   PasswordAuthentication no',
-    match => '^  PasswordAuthentication',
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
