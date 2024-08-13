d5725d39206d82e2af752c081de8e4ad40c10989
eioforkansi <eioforkansi@gmail.com>
2024-08-13 20:10:26 +0100

    good

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+0-subs.py
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    - Function that queries the Reddit API and
+    - returns the number of subscribers (not active users, total subscribers)
+    - for a given subreddit.
+    - If an invalid subreddit is given,
+    - the function should return 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "Reddit_subscriber_script"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception:
+        return 0
+
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+API advanced Project. It involves learning Python, Scripting, Back-end and API
