8dd6974fb269df03155ec7f3f48b30180237273b
eioforkansi <eioforkansi@gmail.com>
2024-08-14 13:08:23 +0100

    Updated

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -29,7 +29,8 @@ def count_words(subreddit, word_list, after='', word_count=None):
     params = {"after": after} if after else {}
 
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
 
         if response.status_code != 200:
             return
@@ -40,19 +41,19 @@ def count_words(subreddit, word_list, after='', word_count=None):
     after = data.get('after')
     posts = data.get('children')
 
-
-
     for post in posts:
         title = post['data']['title'].lower().split()
         for word in word_list:
             lower_word = word.lower()
-            word_count[lower_word] = word_count.get(lower_word, 0) + title.count(lower_word)
+            word_count[lower_word] = word_count.get(lower_word, 0)
+            + title.count(lower_word)
 
     if after:
         count_words(subreddit, word_list, after, word_count)
 
     if after is None:
-        sorted_words = sorted(word_count.items(), key=lambda kv: (-kv[1], kv[0]))
+        sorted_words = sorted(word_count.items(),
+                              key=lambda kv: (-kv[1], kv[0]))
         for word, count in sorted_words:
             if count > 0:
                 print(f'{word}: {count}')
