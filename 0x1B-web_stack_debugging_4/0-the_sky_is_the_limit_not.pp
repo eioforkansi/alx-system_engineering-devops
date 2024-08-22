f1d9a625753caa9850178998d8f89788a9171dc1
eioforkansi <eioforkansi@gmail.com>
2024-08-22 14:03:07 +0100

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,9 @@
+# Increases the amount of traffic an Nginx server can handle.
+
 # Increase the file descriptor limit for Ngnix
 exec {'increase-ulimit-for-nginx':
     command => 'sed -i "s/15/4096/" /etc/default/nginx',
-    path    => '/usr/local/bin/:/bin:/usr/bin'
+    path    => '/usr/local/bin/:/bin/'
 }
 
 # Restart Nginx service
