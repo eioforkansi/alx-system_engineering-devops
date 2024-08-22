5f107a1b29aa6030a3e08fa4482e0c16eee9dd51
eioforkansi <eioforkansi@gmail.com>
2024-08-22 14:13:13 +0100

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,12 @@
 
 # Increase the file descriptor limit for Ngnix
 exec {'increase-ulimit-for-nginx':
-    command => 'sed -i "s/15/4096/" /etc/default/nginx',
+    command => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
     path    => '/usr/local/bin/:/bin/'
-} ->
+}
 
 # Restart Nginx service
 exec {'nginx-restart':
-    command => 'service nginx restart',
+    command => 'sudo service nginx restart',
     path    => '/etc/init.d'
 }
